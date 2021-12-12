6ffcc69449a23a9daf64ef207cabefd11e46fe47
Ben // Zerofour // 04 <60815764+Zerofour04@users.noreply.github.com>
2021-12-12 21:27:25 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
 
+author 'derl30n'
 description 'ESX Pursuit Helper'
 
-client_script { "client/main.lua" }
-
-server_scripts { "server/main.lua" }
+client_script 'client/main.lua'
+server_script 'server/main.lua'
 
 ui_page('client/html/index.html')
 
